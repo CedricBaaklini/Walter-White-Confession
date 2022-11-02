36b6aaa2d2df4e5efa3ae964c7fc152df4f67dbf
CedricBaaklini <77132080+CedricBaaklini@users.noreply.github.com>
2022-11-02 13:10:45 -0400

    Add files via upload

---
+++ confession.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+echo "My name is Walter Hartwell White."
+
+sleep 5
+
+echo "I live at 308 Negra Arroyo Lane, Alburqerque, New Mexico, 87104."
+
+sleep 5
+
+echo "This is my confession."
+
+sleep 5
+
+echo "If you are watching this tape, I'm probably dead..."
+
+sleep 5
+
+echo "murdered by my brother-in-law, Hank Schrader."
+
+sleep 5
+
+echo "Hank has been building a meth empire for over a year now, and using me as a chemist."
+
+sleep 5
+
+echo "Shortly after my fiftieth birthday, Hank came to me with a rather shocking proposition."
+
+sleep 5
+
+echo "He asked that I use my chemistry knowledge to cook methamphetamine, which he would then sell using his connections in tthe drug world, connections that he made through his career with the DEA."
+
+sleep 5
+
+echo "I was astounded."
+
+sleep 5
+
+echo "I always thought Hank was a very moral man, and I was particularly vulnerable at the time."
+
+sleep 5
+
+echo "Something he knew and took advantage of."
+
+sleep 5
+
+echo "I was reeling from a cancer diagnosis that was poised to bankrupt my family."
+
+sleep 5
+
+echo "Hank took me on a ride-along and showed me just how much money even a small meth operation could make."
+
+sleep 5
+
+echo "And I was weak."
+
+sleep 5
+
+echo "I didn't want my family to go into financial ruin, so I agreed."
+
+sleep 5
+
+echo "Every day, I think back with regret."
+
+sleep 5
+
+echo "I quickly realized that I was in way over my head, and Hank had a partner named Gustavo Fring, a businessman."
+
+sleep 5
+
+echo "Hank sold me into servitude to this man, and when I tried to quit, Fring threatened my family."
+
+sleep 5
+
+echo "I didn't know where to turn."
+
+sleep 5
+
+echo "Eventually, Hank and Fring had a falling out."
+
+sleep 5
+
+echo "From what I can gather, Hank was always pushing for a greater share of the business, of which Fring flatly refused to give it, and things escalated."
+
+sleep 5
+
+echo "Fring was able to arrange, uh I guess..."
+
+sleep 5
+
+echo "I guess you call it a hit on my brother-in-law, and failed, but Hank was seriously injured, and I wound up paying his mmedical bills, which amounted to a litte over $177,000."
+
+sleep 5
+
+echo "Upon recovery, Hank was bent on revenge."
+
+sleep 5
+
+echo "Working with a man named Hector Salamanca, he plotted to kill Fring, and did so."
+
+sleep 5
+
+echo "In fact, the bomb that he used was built by me."
+
+sleep 5
+
+echo "And he gave me no option in that."
+
+sleep 5
+
+echo "I have often contemplated suicide, but I'm a coward."
+
+sleep 5
+
+echo "I wanted to go to the police, but I was frightened."
+
+sleep 5
+
+echo "Hank had risen in the reigns to become head of the Albuquerque DEA, and about that time to keep me in line, he took my children from me, for three months he kept him."
+
+sleep 5
+
+echo "My wife, who up until that point, had no idea of my criminal activities, was horrified to learn what I had done."
+
+sleep 5
+
+echo "Why Hank had taken our children."
+
+sleep 5
+
+echo "We were scared."
+
+sleep 5
+
+echo "And I was in Hell."
+
+sleep 5
+
+echo "I hated myself for what I had brought upon my family."
+
+sleep 5
+
+echo "Recently, I tried once again to quit to end this nightmare, and in response, he gave me this."
+
+sleep 5
+
+echo "I can't take this anymore."
+
+sleep 5
+
+echo "I live in fear every day that Hank will kill me."
+
+sleep 5
+
+echo "Or worse, hurt my family."
+
+sleep 5
+
+echo "All I could think to do was to make this video and hope that the world will finally see this man for what he really is."
+
+sleep 5
+
+done
